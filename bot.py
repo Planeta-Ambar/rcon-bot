f0f9a090a73806d0d14467eb875df169e2adb6d3
pereirarodrigo <pereirarodrigocs@gmail.com>
2024-06-29 19:50:58 -0300

    Updates para o uptime to bot

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os
+import time
 
 import discord
 
@@ -17,6 +17,25 @@ async def on_ready():
     await ninho_functions.tree.sync(guild = discord.Object(id = 1196189171530870795))
 
     print("Pronto")
+
+    start = time.time()
+    dur_em_segundos = 59 * 60
+
+    # Roda o bot por 59 minutos
+    # Após isso, ele será reiniciado
+    while True:
+        temp_atual = time.time()
+        temp_passado = temp_atual - start
+
+        if temp_passado == dur_em_segundos - (5 * 60):
+            print(f"Atenção: o bot será reiniciado em 5 minutos!")
+    
+        if temp_passado >= dur_em_segundos:
+            print("O bot está reiniciando.")
+            
+            break
+    
+        time.sleep(1)  # Previne alto uso de CPU
 
 
 config.client.run(config.TOKEN)
